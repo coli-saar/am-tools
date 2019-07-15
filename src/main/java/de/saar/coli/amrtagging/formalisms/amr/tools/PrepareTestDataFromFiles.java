14d0a3ec4fa31e2a5b0758f9f6e23484e69ca923
namednil <mlinde@coli.uni-saarland.de>
2019-07-15 23:18:22 +0200

    Test/Dev data preparation AMR.

--- src/main/java/de/saar/coli/amrtagging/formalisms/amr/tools/PrepareTestDataFromFiles.java
+++ src/main/java/de/saar/coli/amrtagging/formalisms/amr/tools/PrepareTestDataFromFiles.java
@@ -9,9 +9,18 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import de.saar.coli.amrtagging.ConllEntry;
 import de.saar.coli.amrtagging.ConllSentence;
+import de.saar.coli.amrtagging.Util;
 import static de.saar.coli.amrtagging.formalisms.amr.tools.DependencyExtractorCLI.LITERAL_JOINER;
+import de.saar.coli.amrtagging.formalisms.amr.tools.preproc.MrpPreprocessedData;
+import de.saar.coli.amrtagging.formalisms.amr.tools.preproc.NamedEntityRecognizer;
+import de.saar.coli.amrtagging.formalisms.amr.tools.preproc.PreprocessedData;
+import de.saar.coli.amrtagging.formalisms.amr.tools.preproc.PreprocessingException;
+import de.saar.coli.amrtagging.formalisms.amr.tools.preproc.StanfordNamedEntityRecognizer;
+import de.saar.coli.amrtagging.formalisms.amr.tools.preproc.UiucNamedEntityRecognizer;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.simple.Sentence;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -19,6 +28,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -27,10 +38,10 @@ import java.util.List;
 public class PrepareTestDataFromFiles {
     
     @Parameter(names = {"--corpusPath", "-c"}, description = "Path to the input corpus") //required = true)
-    private String corpusPath = "/home/matthias/uni/multi-amparser/eval_tools/AMR/2015/cleanamrdata2019/2019rerun/metadata/";
+    private String corpusPath = "/home/matthias/Schreibtisch/Hiwi/Koller/MRP/data/training/our-amr/second_shot/evalDev/";
     
     @Parameter(names = {"--outPath", "-o"}, description = "Prefix for output files")//, required = true)
-    private String outPath = "/home/matthias/Schreibtisch/Hiwi/Koller/Datensets_sammeln/AMR-toy/";
+    private String outPath = "/home/matthias/Schreibtisch/Hiwi/Koller/MRP/data/training/our-amr/second_shot/evalDev/";
          
    @Parameter(names={"--prefix","-p"}, description = "Prefix for output file names (e.g. dev --> dev.amconll)")//, required=true)
     private String prefix = "dev";
@@ -38,9 +49,15 @@ public class PrepareTestDataFromFiles {
     @Parameter(names = {"--help", "-?"}, description = "displays help if this is the only command", help = true)
     private boolean help = false;
     
-    public static HashSet<String> specialTokens = new HashSet<>(Arrays.asList(new String[]{"_name_", "_number_", "_date_"}));
+    @Parameter(names = {"--companion"}, description = "Path to MRP companion data (will disable builtin lemmatization)", required = false)
+    private String companionDataFile = null;
+
+    @Parameter(names = {"--stanford-ner-model"}, description = "Filename of Stanford NER model english.conll.4class.distsim.crf.ser.gz; if argument is not given, use UIUC NER tagger")
+    private String stanfordNerFilename = null;
     
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    //public static HashSet<String> specialTokens = new HashSet<>(Arrays.asList(new String[]{"_name_", "_number_", "_date_"}));
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         
         PrepareTestDataFromFiles cli = new PrepareTestDataFromFiles();
         JCommander commander = new JCommander(cli);
@@ -61,21 +78,44 @@ public class PrepareTestDataFromFiles {
         List<List<String>> sentences = readFile(cli.corpusPath+"/sentences.txt");
         List<List<String>> literals = readFile(cli.corpusPath+"/literal.txt");
         List<List<String>> posTags = readFile(cli.corpusPath+"/pos.txt");
+        List<List<String>> IDs = readFile(cli.corpusPath+"/graphIDs.txt");
         
         if (sentences.size() != literals.size() || literals.size() != posTags.size()){
             System.err.println("Inputs seem to have different lengths: "+sentences.size()+" "+literals.size()+posTags.size());
             return;
         }
         
+        PreprocessedData preprocData = null;
+        NamedEntityRecognizer neRecognizer = null;
+
+        if( cli.companionDataFile != null ) {
+            preprocData = new MrpPreprocessedData(new File(cli.companionDataFile));
+        } else {
+            // It would be nice to encapsulate the choice between MRP preproc data and
+            // Stanford preproc data as neatly as elsewhere, but as I read the code below,
+            // the Stanford lemmatizer is applied to the expandedWords, in which tokens
+            // have been split apart at underscores. Constructing a StanfordPreprocData
+            // up here would only duplicate the code. - AK, July 2019.
+        }
+
+        if( cli.stanfordNerFilename != null ) {
+            neRecognizer = new StanfordNamedEntityRecognizer(new File(cli.stanfordNerFilename));
+        } else {
+            neRecognizer = new UiucNamedEntityRecognizer();
+        }
+
+        
+        
         List<ConllSentence> output = new ArrayList<>();
         for (int i = 0; i < sentences.size();i++){
             ConllSentence o = new ConllSentence();
-            
+            String graphID = IDs.get(i).get(0);
             List<String> expandedWords = new ArrayList<>();
             List<Integer> origPositions = new ArrayList<>();
             List<String> ners = new ArrayList<>();
             for (int id = 0; id < sentences.get(i).size();id++){
-                ConllEntry e = new ConllEntry(id+1, literals.get(i).get(id));
+                String wordForm = literals.get(i).get(id).replace(LITERAL_JOINER, " ");
+                ConllEntry e = new ConllEntry(id + 1, wordForm);
                 o.add(e);
                 ners.add("O");
                 for (String w : literals.get(i).get(id).split(LITERAL_JOINER)){
@@ -83,23 +123,37 @@ public class PrepareTestDataFromFiles {
                     origPositions.add(id);
                 }
             }
-            Sentence stanfSent = new Sentence(expandedWords);
-            List<String> nerTags = stanfSent.nerTags();
-            List<String> ourLemmas = new ArrayList<>(o.words());
-            List<String> lemmas = stanfSent.lemmas();
-            for (int j = 0; j < lemmas.size(); j++){
-                ners.set(origPositions.get(j), nerTags.get(j));
-                ourLemmas.set(origPositions.get(j), lemmas.get(j));
-                
-            }
-            o.addReplacement(sentences.get(i));
-            o.addPos(posTags.get(i));
-            System.err.println(literals.get(i));
-            System.err.println(ourLemmas);
-            o.addLemmas(ourLemmas);
-            o.addNEs(ners);
+            List<String> lemmas;
+            List<CoreLabel> nerTags;
+            try {
+                if( preprocData != null ) {
+                    lemmas = preprocData.getLemmas(graphID);
+                    nerTags = neRecognizer.tag(preprocData.getTokens(graphID));
+                } else {
+                    Sentence stanfSent = new Sentence(expandedWords);
+                    lemmas = stanfSent.lemmas();
+                    nerTags = neRecognizer.tag(Util.makeCoreLabelsForTokens(expandedWords));
+                }
             
-            output.add(o);
+                List<String> ourLemmas = new ArrayList<>(o.words());
+                for (int j = 0; j < lemmas.size(); j++){
+                    ners.set(origPositions.get(j), nerTags.get(j).ner());
+                    ourLemmas.set(origPositions.get(j), lemmas.get(j));
+
+                }
+                o.addReplacement(sentences.get(i));
+                o.addPos(posTags.get(i));
+                System.err.println(literals.get(i));
+                System.err.println(ourLemmas);
+                o.addLemmas(ourLemmas);
+                o.addNEs(ners);
+                o.setId(graphID);
+
+                output.add(o);
+            } catch (Exception ex) { //PreprocessingException
+                System.err.println("Ignoring exception");
+                ex.printStackTrace();
+             }
             
         }
         
