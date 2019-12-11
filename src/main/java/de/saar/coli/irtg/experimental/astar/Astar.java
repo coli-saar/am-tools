bccf53161e3c279a308224a24ef1f915819b1200
Alexander Koller <koller@ling.uni-potsdam.de>
2019-12-11 18:27:43 +0100

    stanford agenda for a*

--- src/main/java/de/saar/coli/irtg/experimental/astar/Astar.java
+++ src/main/java/de/saar/coli/irtg/experimental/astar/Astar.java
@@ -111,8 +111,8 @@ public class Astar {
         this.typeLexicon = typeLexicon; // new AMAlgebraTypeInterner(types, edgeLabelLexicon);  // <--- TODO: this is expensive for some reason
         w.record();
 
-        //this.outside = new StaticOutsideEstimator(edgep, tagp);
-        this.outside = new DynamicOutsideEstimator(edgep, tagp);
+        this.outside = new StaticOutsideEstimator(edgep, tagp);
+//        this.outside = new DynamicOutsideEstimator(edgep, tagp);
 //        this.outside = new SupertagOnlyOutsideEstimator(tagp);
 //        this.outside = new TrivialOutsideEstimator();
 
@@ -180,7 +180,8 @@ public class Astar {
 
         w.record();
 
-        Agenda agenda = new PriorityQueueAgenda(declutterAgenda);
+//        Agenda agenda = new PriorityQueueAgenda(declutterAgenda);
+        Agenda agenda = new StanfordAgenda();
 
         SiblingFinder[] siblingFinders = new SiblingFinder[edgeLabelLexicon.size()];  // siblingFinders[op] = type-sibling-finder for operation op, for all seen types
         for (int j : edgeLabelLexicon.getKnownIds()) {
@@ -349,8 +350,8 @@ public class Astar {
 
     private Item makeSkipItem(Item originalItem, int newStart, int newEnd, int skippedPosition) {
         double nullProb = tagp.get(skippedPosition, tagp.getNullSupertagId());        // log P(supertag = NULL | skippedPosition)
-        //double ignoreProb = edgep.get(0, skippedPosition, edgep.getIgnoreEdgeId());   // log P(inedge = IGNORE from 0 | skippedPosition)
-        double ignoreProb = 0;
+        double ignoreProb = edgep.get(0, skippedPosition, edgep.getIgnoreEdgeId());   // log P(inedge = IGNORE from 0 | skippedPosition)
+//        double ignoreProb = 0;
 
         if (nullProb + ignoreProb < FAKE_NEG_INFINITY / 2) {
             // either NULL or IGNORE didn't exist - probably IGNORE
