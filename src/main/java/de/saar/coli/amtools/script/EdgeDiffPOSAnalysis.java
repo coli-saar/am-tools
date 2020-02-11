f14758f19356279332e5c6577d1b9a8afa1d70d5
Jonas Groschwitz <jonasg@coli.uni-saarland.de>
2020-02-11 16:40:40 +0100

    added label analysis to POS analysis

--- src/main/java/de/saar/coli/amtools/script/EdgeDiffPOSAnalysis.java
+++ src/main/java/de/saar/coli/amtools/script/EdgeDiffPOSAnalysis.java
@@ -10,13 +10,16 @@ import com.beust.jcommander.Parameter;
 import com.google.common.collect.Sets;
 import de.saar.basic.Pair;
 import de.up.ling.irtg.util.Counter;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import se.liu.ida.nlp.sdp.toolkit.graph.Edge;
 import se.liu.ida.nlp.sdp.toolkit.graph.Graph;
 import se.liu.ida.nlp.sdp.toolkit.graph.Node;
 import se.liu.ida.nlp.sdp.toolkit.io.GraphReader2015;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -26,16 +29,16 @@ import java.util.stream.Collectors;
  */
 public class EdgeDiffPOSAnalysis {
      @Parameter(names = {"--corpus", "-c"}, description = "Path to the primary input corpus")//, required = true)
-    private String corpusPath = "../../data/corpora/semDep/sdp2014_2015/data/2015/en.dm.sdp";
+    private String corpusPath = "../../data/corpora/semDep/sdp2014_2015/data/2015/en.psd.sdp";
 
     @Parameter(names = {"--corpus2", "-c2"}, description = "Path to the secondary input corpus")//, required = true)
-    private String corpusPath2 = "../../data/corpora/semDep/sdp2014_2015/data/2015/en.psd.sdp";
+    private String corpusPath2 = "../../data/corpora/semDep/sdp2014_2015/data/2015/en.dm.sdp";
 
     
     @Parameter(names = {"--help", "-?","-h"}, description = "displays help if this is the only command", help = true)
     private boolean help=false;
-   
 
+    private static Map<Pair<String, String>, Counter<String>> posPair2EdgeLabel = new HashMap<>();
     
     public static void main(String[] args) throws IOException {
         //just getting command line args
@@ -65,8 +68,14 @@ public class EdgeDiffPOSAnalysis {
         Counter<String> sourceNodePOSCounter = new Counter<>();
         Counter<String> targetNodePOSCounter = new Counter<>();
 
+
+
         while ((sdpGraph = gr.readGraph()) != null && (sdpGraph2 = gr2.readGraph()) != null){
             totalEdges1 += sdpGraph.getEdges().size();
+            Map<Pair<Integer, Integer>, String> edgeToLabel1 = new HashMap<>();
+            for (Edge e : sdpGraph.getEdges()) {
+                edgeToLabel1.put(encodeEdge(e), e.label);
+            }
             Set<Pair<Integer, Integer>> edges1 = sdpGraph.getEdges().stream().map(e -> encodeEdge(e)).collect(Collectors.toSet());
             Set<Pair<Integer, Integer>> edges2 = sdpGraph2.getEdges().stream().map(e -> encodeEdge(e)).collect(Collectors.toSet());
             for (Pair<Integer, Integer> edge : edges1) {
@@ -76,7 +85,10 @@ public class EdgeDiffPOSAnalysis {
                 //if (edges2.contains(new Pair(edge.right, edge.left))) {
                     Node sourceNode = sdpGraph.getNode(edge.left);//POS is the same in both graphs, sdpGraph2 would give same result for POS tag.
                     Node targetNode = sdpGraph.getNode(edge.right);//POS is the same in both graphs, sdpGraph2 would give same result for POS tag.
-                    posPairCounter.add(new Pair(sourceNode.pos, targetNode.pos));
+                    Pair<String, String> posPair = new Pair<>(sourceNode.pos, targetNode.pos);
+                    posPairCounter.add(posPair);
+                    incLabel(posPair, edgeToLabel1.get(edge));
+
                     sourceNodePOSCounter.add(sourceNode.pos);
                     targetNodePOSCounter.add(targetNode.pos);
                 }
@@ -85,13 +97,35 @@ public class EdgeDiffPOSAnalysis {
 
         System.err.println("total edges in graphbank 1: "+totalEdges1);
         System.err.println("total edge differences of this kind found: "+sourceNodePOSCounter.sum());
+        System.err.println("source POS tags:");
         sourceNodePOSCounter.printAllSorted();
         System.err.println();
+        System.err.println("target POS tags:");
         targetNodePOSCounter.printAllSorted();
         System.err.println();
+        System.err.println("POS pairs (source, target):");
         posPairCounter.printAllSorted();
+        System.err.println();
+        System.err.println("Most common edge labels:");
+        int i = 0;
+        for (Object2IntMap.Entry<Pair<String, String>> posPairEntry : posPairCounter.getAllSorted()) {
+            i++;
+            if (i >= 20) {
+                break;
+            }
+            System.err.println(posPairEntry);
+            posPair2EdgeLabel.get(posPairEntry.getKey()).printAllSorted();
+        }
     }
 
+    private static void incLabel(Pair<String, String> posPair, String label) {
+        Counter<String> counter = posPair2EdgeLabel.get(posPair);
+        if (counter == null) {
+            counter = new Counter<>();
+            posPair2EdgeLabel.put(posPair, counter);
+        }
+        counter.add(label);
+    }
 
     private static Pair<Integer, Integer> encodeEdge(Edge e) {
         return new Pair(e.source, e.target);
