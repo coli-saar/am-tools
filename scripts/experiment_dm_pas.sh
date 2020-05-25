440faa4a91d0be65c3a7b195f5ebef75cda511a6
Alexander Koller <koller@ling.uni-potsdam.de>
2020-05-25 11:17:43 +0200

    fresh typecaches for each scores.zip

--- scripts/experiment_dm_pas.sh
+++ scripts/experiment_dm_pas.sh
@@ -10,7 +10,7 @@ THREADS=${THREADS:-2}
 
 # prepwork
 
-rm -f /tmp/typecache.dat
+#rm -f /tmp/typecache.dat
 HOSTNAME=`hostname`
 
 for corpus in "${@:2}" # all except first
@@ -28,14 +28,13 @@ do
     do
 	csv=${corpus//\//_}.csv
 
-	output=$($JAVA_HOME/bin/java $JAVA_OPTIONS -cp build/libs/am-tools.jar de.saar.coli.amtools.astar.Astar --print-data -s $CORPUS_BASE/$corpus/scores.zip --typecache /tmp/typecache.dat --threads $THREADS --outside-estimator $heuristic --statistics $corpus/$heuristic.csv -o $corpus )
+	output=$($JAVA_HOME/bin/java $JAVA_OPTIONS -cp build/libs/am-tools.jar de.saar.coli.amtools.astar.Astar --print-data -s $CORPUS_BASE/$corpus/scores.zip --typecache $CORPUS_BASE/$corpus/typecache.dat --threads $THREADS --outside-estimator $heuristic --statistics $corpus/$heuristic.csv -o $corpus )
 	last_line=${output##*$'\n'}
 	amconll=$(echo "$last_line"|cut -f4)
 
 	eval=$(./scripts/evaluate_dm_pas.sh $amconll out $GOLD_SDP|grep "F "|head -1)
 	f=$(echo "$eval"|cut -f2 -d " ")
 	echo -e "$corpus\t$HOSTNAME\t$heuristic\t$f\t$last_line" >> $corpus/eval.tsv
-	exit 0
     done
 done
 
