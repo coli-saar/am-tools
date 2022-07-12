3314bc4c4604906f692ba025f95c404b07ab580c
jgroschwitz <jonasg@coli.uni-saarland.de>
2022-07-12 12:56:40 +0100

    Update VisualizeFromAmconll.java

--- src/main/java/de/saar/coli/amtools/analysis/VisualizeFromAmconll.java
+++ src/main/java/de/saar/coli/amtools/analysis/VisualizeFromAmconll.java
@@ -15,6 +15,34 @@ import java.util.Collection;
 import java.util.List;
 
 public class VisualizeFromAmconll {
+    
+    /**
+    Parameters are as in de.saar.coli.amtools.evaluation.EvaluateAMConll. However, the --gold parameter is not used. The --evaluationToolset parameter
+    is not required, unless you want to apply postprocessing. (You may want to use de.saar.coli.amtools.evaluation.toolsets.AMREvaluationToolset for AMR.)
+    
+    @Parameter(names = {"--corpus", "-c"}, description = "Path to the input corpus with decoded AM dependency trees", required = true)
+    private String corpusPath = null;
+
+    @Parameter(names = {"--outPath", "-o"}, description = "Path for output files", required = true)
+    public String outPath = null;
+
+    @Parameter(names = {"--gold", "-g"}, description = "Path to gold corpus. Usually expected to contain the same instances in the same order as " +
+            "the --corpus file (unless the evaluation toolset says otherwise). Giving the gold corpus here is optional, and only works if the evaluation" +
+            "toolset has the compareToGold function implemented. Alternatively, use an external evaluation tool after this program has run (such as" +
+            "the Smatch script for AMR graphs).")
+    private String goldCorpus = null;
+
+    @Parameter(names = {"--evaluationToolset", "-et"}, description = "Classname of the EvaluationToolset class to be used. Default applies no postprocessing and writes the files in ISI AMR format")
+    private String evaluationToolsetName = "de.saar.coli.amtools.evaluation.toolsets.EvaluationToolset";
+
+    @Parameter(names = {"--extras", "-e"}, description = "Additional parameters to the constructor of the Evaluation toolset, as a single string. Optional." +
+            " Note that using this parameter causes a different constructor of the evaluation toolset to be called.")
+    private String toolsetExtras = null;
+
+    @Parameter(names = {"--help", "-?","-h"}, description = "displays help if this is the only command", help = true)
+    private boolean help=false;
+    
+    **/
 
     public static void main(String[] args) throws IOException, ParseException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ParserException, AlignedAMDependencyTree.ConllParserException, InterruptedException {
 
