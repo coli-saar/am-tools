1a76faf83bbe319f92ab076ac5353d23fec0a333
meaghan <meaghanfowlie@gmail.com>
2022-07-21 12:54:49 +0200

    merged with compatable_with_morpheme_splitter. de.saar.coli.amtools.decomposition.analysis.MakeAMConllFromLeamrAltoCorpus should now be replaceable by the more general de.saar.coli.amtools.decomposition.analysis..MakeAMConllFromAltoCorpus but I didn't delete the former.

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ Compile the am-tools as follows:
 The `./gradlew` is for Mac and Linux; on Windows, use `gradlew` instead.
 
 This should download a bunch of dependencies and finally create a file `build/lib/am-tools.jar`, which will be quite big because it contains pretrained models for the Stanford CoreNLP system.
+
+
+## Analysis and visualisation
+
+A number of tools are available for error analysis and visualisation. Here are some:
+
+### Find graphs that are non-decomposable because of the multiple roots problem
+
+Class to use:
+`de.saar.coli.amrtagging.formalisms.amr.tools.datascript.RawAMRCorpus2TrainingData`
+
+To see usage from the command line, assuming your `am-tools.jar` is in `build/libs/`:
+
+```bash
+java -cp build/libs/am-tools.jar de.saar.coli.amrtagging.formalisms.amr.tools.datascript.RawAMRCorpus2TrainingData -?
+```
+
--- src/main/java/de/saar/coli/amtools/decomposition/analysis/CountSupertags.java
+++ src/main/java/de/saar/coli/amtools/decomposition/analysis/CountSupertags.java
@@ -5,9 +5,12 @@
  */
 package de.saar.coli.amtools.decomposition.analysis;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import de.saar.basic.Pair;
 import de.saar.coli.amrtagging.AmConllEntry;
 import de.saar.coli.amrtagging.AmConllSentence;
+import de.saar.coli.amrtagging.formalisms.amr.tools.datascript.RawAMRCorpus2TrainingData;
 import de.up.ling.irtg.algebra.ParserException;
 import de.up.ling.irtg.algebra.graph.ApplyModifyGraphAlgebra.Type;
 import de.up.ling.irtg.algebra.graph.SGraph;
@@ -20,10 +23,21 @@ import java.util.*;
 
 /**
  *
- * @author mego
+ * @author meaghan
  */
 public class CountSupertags {
 
+    @Parameter(names = {"--inputPath", "-i"}, description = "Path to amconll file to be analysed", required = true)
+    private String inputPath;
+
+    @Parameter(names = {"--outputParentPath", "-o"}, description = "Path to write output to. Will create and use subfolder /corpusName/analysis/supertags/ where corpusName is taken from the amconll filename", required = true)
+    private String outputParentPath;
+
+    @Parameter(names = {"--keepSources", "-k"}, description = "Flag; if set, supertags with the same shape but different sources are counted as different")
+    private boolean keepSources = false;
+
+    @Parameter(names = {"--help", "-?", "-h"}, description = "displays help if this is the only command", help = true)
+    private boolean help = false;
 
     /**
      * Given an amconll file of am-parser output, counts the supertags assigned
@@ -40,19 +54,43 @@ public class CountSupertags {
         // Change this as needed
         // input will be pathPrefix + corporaFolder + corpus.amconll  (e.g. ... /training/AMR4.amconll)
         // output will be pathPrefix + /analysis/ + corporaFolder + corpus + /supertags/ (e.g. ... /analysis/training/AMR4/supertags/)
-        String corpus = "COGS_amconll_list_train_epoch75"; //args[2];  // AMR
-        String corporaFolder = ""; //args[1];  //EM
-        String pathPrefix = "temp/"; //args[0];  //"/home/mego/Documents/amconll_files/";
+        // String corpus = "COGS_amconll_list_train_epoch75"; //args[2];  // AMR
+        // String corporaFolder = ""; //args[1];  //EM
+        // String pathPrefix = "temp/"; //args[0];  //"/home/mego/Documents/amconll_files/";
 
-        String heading = "Supertags sorted by graph constant in " + corporaFolder + "/" + corpus;
-        String outpath = pathPrefix + "analysis/" + corporaFolder  + "/" + corpus + "/supertags/";
+        CountSupertags counter = new CountSupertags();
 
+        JCommander commander = new JCommander(counter);
+        commander.setProgramName("viz");
+
+        try {
+            commander.parse(args);
+        } catch (com.beust.jcommander.ParameterException ex) {
+            System.err.println("An error occurred: " + ex);
+            System.err.println("\n Available options: ");
+            commander.usage();
+            return;
+        }
+
+        if (counter.help) {
+            commander.usage();
+            return;
+        }
+
+//        String inputPath = args[0];
+//        String outputParentPath = args[1];
+
+        String[] pathArray = counter.inputPath.split("/");
+        String[] fileNameArray = pathArray[pathArray.length -1].split("\\.");
+        String corpus = fileNameArray[0];
+
+        String heading = "Supertags sorted by graph constant in " + counter.inputPath;
+        String outPath = counter.outputParentPath + "/" + corpus + "/analysis/" + "/supertags/";
 
         // read in the file and make it into a list of type AmConllSentence
-        String amconllFilePath = pathPrefix + corporaFolder  + "/" + corpus + ".amconll";
-        List<AmConllSentence> amConllSentences = AmConllSentence.readFromFile(amconllFilePath);
+        List<AmConllSentence> amConllSentences = AmConllSentence.readFromFile(counter.inputPath);
 
-        System.err.println("Counting supertags for graphs in " + amconllFilePath + "\n");
+        System.err.println("Counting supertags for graphs in " + counter.inputPath + "\n");
 
         // a map for storing the graphs and their types. With each graph we store the sentence
         Map<SGraph, Map<Pair<String, Type>, List<AmConllSentence>>> map = new HashMap<>();
@@ -60,13 +98,17 @@ public class CountSupertags {
         // for every word in the corpus, add the graphs and supertags to map
         for (AmConllSentence sent : amConllSentences) {
             for (AmConllEntry word : sent) {
-                // delexicalise and remove all sources but the root
-                SGraph noSourceGraph = word.delexGraph();
-                noSourceGraph = noSourceGraph.forgetSourcesExcept(Collections.singleton("root"));
-                noSourceGraph.setEqualsMeansIsomorphy(true); // to ignore node names in the equality check
+                // delexicalise
+                SGraph delexGraph = word.delexGraph();
+                if (!counter.keepSources) {
+                    // remove all sources but the root
+                    delexGraph = delexGraph.forgetSourcesExcept(Collections.singleton("root"));
+                }
+                // ignore node names in the equality check
+                delexGraph.setEqualsMeansIsomorphy(true);
                 // get the current counter for this graph, making it if it doesn't exist yet
                 Map<Pair<String, Type>, List<AmConllSentence>> mapForNoSourceGraph =
-                        map.computeIfAbsent(noSourceGraph, k -> new HashMap<>());
+                        map.computeIfAbsent(delexGraph, k -> new HashMap<>());
                 Pair<String, Type> supertag = new Pair<>(word.getDelexSupertag(), word.getType());
                 List<AmConllSentence> currentExamples =
                         mapForNoSourceGraph.computeIfAbsent(supertag, k -> new ArrayList<>());
@@ -76,10 +118,10 @@ public class CountSupertags {
         }
 
         System.out.println("Writing summary");
-        CountSources.writeSummary(map, outpath, heading);
+        CountSources.writeSummary(map, outPath, heading);
 
         System.out.println("Writing examples");
-        CountSources.writeExamples(map, outpath, true);
+        CountSources.writeExamples(map, outPath, true);
 
 
     }
